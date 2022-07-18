931db7303c4b38f27c2334a7125043c4eba5ffe6
Koenig, Jennifer <jennifer.koenig@sva.de>
2022-07-18 13:32:54 +0200

    Create service principals for resource creation and service connection; use Azure Key Vault for variable storage

--- setup/terraform.tf
+++ setup/terraform.tf
@@ -2,18 +2,18 @@ terraform {
   required_providers {
     azuredevops = {
       source  = "microsoft/azuredevops"
-      version = ">=0.1.0" #0.2.1
+      version = ">=0.2.1"
     }
 
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0" #3.14.0
+      version = "~> 3.14.0"
 
     }
 
     azuread = {
       source = "hashicorp/azuread"
-      version = "~> 1.0" #2.26.1
+      version = "~> 2.26.1"
     }
 
   }
