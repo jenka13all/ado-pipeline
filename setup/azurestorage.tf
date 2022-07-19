701b1bf53ffe03e66183c3847a1038f2cb167d39
Koenig, Jennifer <jennifer.koenig@sva.de>
2022-07-19 13:09:09 +0200

    add queue logging to storage account

--- setup/azurestorage.tf
+++ setup/azurestorage.tf
@@ -13,6 +13,15 @@ resource "azurerm_storage_account" "sa" {
   location                 = var.az_location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  queue_properties  {
+    logging {
+        delete                = true
+        read                  = true
+        write                 = true
+        version               = "1.0"
+        retention_policy_days = 10
+    }
+  }
 
 }
 
